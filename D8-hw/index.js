3e567850dcabbf22a03ee946e30c7c2c50b0fd62
slavko <slavkoj6@gmail.com>
2021-02-10 21:12:03 +0100

    some refactor

--- D8-hw/index.js
+++ D8-hw/index.js
@@ -2,89 +2,85 @@ const tasklistUL = document.getElementById('tasklist-ul');
 const inputNew = document.getElementById('new-list-item');
 const listBody = document.getElementById('list-body')
 const aToZ = document.getElementById('atoz');
-const zToA =document.getElementById('ztoa')
+const zToA = document.getElementById('ztoa')
 
 
 const addNewItemButton = document.getElementById("add-new-item")
-//console.log(addNewItemButton);
 let listItemArray = [];
 listItemArray.push("buy bread")
 
 
 
 const populateList = function () {
+    listBody.innerHTML = ""
     for (elem of listItemArray) {
-    
-    const newListItemDiv = document.createElement('div');
-    const newListItemLi = document.createElement('li');
-    const trashButton = document.createElement('i')
-    console.log(inputNew.value)
 
-    trashButton.setAttribute("id", "trash-button");
+        const newListItemDiv = document.createElement('div');
+        const newListItemLi = document.createElement('li');
+        const trashButton = document.createElement('i')
+        console.log(inputNew.value)
 
-    //appending
-    listBody.appendChild(newListItemDiv);
-    newListItemDiv.appendChild(newListItemLi);
-    newListItemDiv.appendChild(trashButton);
+        trashButton.setAttribute("id", "trash-button");
+
+        //appending
+        listBody.appendChild(newListItemDiv);
+        newListItemDiv.appendChild(newListItemLi);
+        newListItemDiv.appendChild(trashButton);
 
 
-    //adding classes
-    newListItemDiv.classList.add("list-item", "d-flex", "justify-content-between", "align-items-center", "my-2")
-    newListItemLi.classList.add("fs-4")
-    trashButton.classList.add("fas", "fa-trash", "fa-2x", "btn", "btn-outline")
-    trashButton.addEventListener("click", deleteItemFromList)
+        //adding classes
+        newListItemDiv.classList.add("list-item", "d-flex", "justify-content-between", "align-items-center", "my-2")
+        newListItemLi.classList.add("fs-4")
+        trashButton.classList.add("fas", "fa-trash", "fa-2x", "btn", "btn-outline")
+        trashButton.addEventListener("click", deleteItemFromList)
 
-    //setting value of new list item
-    const textContent = document.createTextNode(elem);
-    newListItemLi.appendChild(textContent);
-    console.log(listBody);
+        //setting value of new list item
+        const textContent = document.createTextNode(elem);
+        newListItemLi.appendChild(textContent);
+        console.log(listBody);
     }
 }
 
 const addNewItemToList = function () {
     console.log(inputNew.value)
-    if (inputNew.value !=="") {
+    if (inputNew.value !== "") {
         listItemArray.push(inputNew.value);
     }
-    
+
     inputNew.value = ""
-    listBody.innerHTML=""
     populateList();
 }
 
 const deleteItemFromList = function (event) {
-    console.log("event",event);
+    console.log("event", event);
     console.log(event.target.previousElementSibling.childNodes[0].nodeValue)
     //event.target.parentNode.parentNode.removeChild(event.target.parentNode)
-    for (let i=0;i<listItemArray.length;i++) {
+    for (let i = 0; i < listItemArray.length; i++) {
         if (event.target.previousElementSibling.childNodes[0].nodeValue === listItemArray[i]) {
-            listItemArray.splice(i,1)
+            listItemArray.splice(i, 1)
         }
     }
-    listBody.innerHTML=""
     populateList();
 }
 
 const sortAtoZ = function () {
     listItemArray.sort();
-    listBody.innerHTML=""
     populateList();
 }
 
 const sortZToA = function () {
     listItemArray.sort();
     listItemArray.reverse();
-    listBody.innerHTML=""
     populateList();
 }
 populateList()
 
-inputNew.addEventListener('keyup', function(event) {
-    if (event.key ==="Enter") {
+inputNew.addEventListener('keyup', function (event) {
+    if (event.key === "Enter") {
         event.preventDefault();
         addNewItemToList();
     }
 })
 addNewItemButton.addEventListener('click', addNewItemToList)
-aToZ.addEventListener("click",sortAtoZ)
-zToA.addEventListener("click",sortZToA)
+aToZ.addEventListener("click", sortAtoZ)
+zToA.addEventListener("click", sortZToA)
