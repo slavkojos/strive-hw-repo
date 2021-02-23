a6ff494e4c667b8adbfb30e0464306120f1ecc91
slavko <slavkoj6@gmail.com>
2021-02-23 13:45:06 +0100

    implement counter for user boards

--- D9-hw/index.js
+++ D9-hw/index.js
@@ -4,7 +4,6 @@ const pickButton = document.getElementById("pick-button");
 const pickedNumbers = [];
 window.onload = () => {
   for (let i = 0; i < 75; i++) {
-    console.log("hey");
     const bingoField = document.createElement("h4");
     bingoBody.appendChild(bingoField);
     bingoField.classList.add("bingo-field", "mx-1");
@@ -18,10 +17,9 @@ function playAudio() {
 }
 
 let duplicateCounter = 0;
-const pickNumber = () => {
+function pickNumber() {
   const bingoField = document.getElementsByClassName("bingo-field");
   if (pickedNumbers.length < 75) {
-    console.log("hello");
     const randomNumber = generateRandomNumber();
     //pickedNumbers.push(randomNumber)
     for (let j = 0; j < pickedNumbers.length; j++) {
@@ -43,9 +41,48 @@ const pickNumber = () => {
   } else {
     alert("All numbers have been picked!");
   }
-};
+}
 
-const generateRandomNumber = () => {
+function generateRandomNumber() {
   return Math.floor(Math.random() * (75 - 1 + 1)) + 1;
-};
+}
 pickButton.addEventListener("click", pickNumber);
+
+// code for starting game and generating user boards
+
+//player counter
+const howManyPlayersContainer = document.getElementById("player-counter");
+const spanCounter = document.getElementById("span-counter");
+const addButton = document.getElementById("add");
+const subtractButton = document.getElementById("subtract");
+const startGameButton = document.getElementById("generate");
+function handleCounter(event) {
+  if (event.target.id === "subtract") {
+    spanCounter.innerText = parseInt(spanCounter.innerText) - 1;
+  } else if (event.target.id === "add") {
+    spanCounter.innerText = parseInt(spanCounter.innerText) + 1;
+  }
+  if (parseInt(spanCounter.innerText) < 3) {
+    subtractButton.style.visibility = "hidden";
+  } else {
+    subtractButton.style.visibility = "visible";
+  }
+  if (parseInt(spanCounter.innerText) >= 10) {
+    addButton.style.visibility = "hidden";
+  } else {
+    addButton.style.visibility = "visible";
+  }
+}
+const startGame = document.getElementById("generate");
+
+addButton.addEventListener("click", handleCounter);
+subtractButton.addEventListener("click", handleCounter);
+startGameButton.addEventListener("click", generateUserBoards);
+
+const userBoards = document.getElementById("user-boards");
+
+function generateUserBoards() {
+  console.log(spanCounter.innerText);
+  howManyPlayersContainer.style.display = "none";
+  const userBoardsRow = document.getElementById("user-boards-row");
+}
